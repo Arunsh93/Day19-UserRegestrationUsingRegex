7f597dcf3b3741ee64961d5424875805cc6fc457
Arunsh93 <Arunhosur93@gmail.com>
2021-10-09 09:57:33 +0530

    Valid First Name using Regex

---
+++ RegexPracticeProblems/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RegexPracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******Welcome to User Regestration******");
+            UserRegestration user = new UserRegestration();
+            Console.Write("Enter First Name: ");
+            string firstName = Console.ReadLine();
+            bool result = user.ValidateFirstName(firstName);
+
+            if(result == true)
+            {
+                Console.WriteLine("Valid Name!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name!");
+            }
+        }
+    }
+}
---
+++ RegexPracticeProblems/UserRegestration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPracticeProblems
+{
+    class UserRegestration
+    {
+        public string RegexFirstName = "[A-Z]{1}[a-z]{2}";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, RegexFirstName);
+        }
+    }
+}
